e2d3ab23b4d2e867825682c7f2acc709aa9e3de8
Vinícius Medeiros Wanderley <viniciusmedeiros2571@gmail.com>
2018-08-27 23:22:27 -0300

    Primeiras alterações no app
    PS: Não é null

--- app/src/main/java/com/neona/numbiosis/MainActivity.java
+++ app/src/main/java/com/neona/numbiosis/MainActivity.java
@@ -1,21 +1,28 @@
 package com.neona.numbiosis;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.neona.numbiosis.metodos.Raiz;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTeste = (Button) findViewById(R.id.btn_teste);
+        btnTeste.setOnClickListener((View.OnClickListener ) this);
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
@@ -31,5 +38,15 @@ public class MainActivity extends AppCompatActivity {
         graph.addSeries(series);
         graph.getViewport().setScalable(true);
         graph.getViewport().setScrollable(true);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btn_teste:
+                Intent it = new Intent(this, TelaInicial.class);
+                startActivity(it);
+                break;
+        }
     }
 }
