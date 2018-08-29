0f00eacc0d69730bb553a4c5656a71f7f9bde7a5
italoalvees <37043793+italoalvees@users.noreply.github.com>
2018-08-29 17:17:21 -0300

    Falsa Posição

--- app/src/main/java/com/neona/numbiosis/FalsaPosição.java
+++ app/src/main/java/com/neona/numbiosis/FalsaPosição.java
@@ -1,6 +1,57 @@
 package com.neona.numbiosis;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class FalsaPosição extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class FalsaPosição extends AppCompatActivity implements  View.OnClickListener {
+    EditText função , a,b,tol,x,n;
+    String funçãoS,aS,bS,tolS,xS,nS;//variaveis para captação dos dados introduzidos pelo usuário
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_falsa_posicao);//tela que estamos usando
+
+        Button button = (Button) findViewById(R.id.button_falsa);//instanciamos o botão da tela
+        button.setOnClickListener((View.OnClickListener ) this); //colocamos ele pra ser "escutado"
+
+        função = (EditText) findViewById(R.id.editText); //instanciamos as caixas de texto da tela
+        a = (EditText) findViewById(R.id.editText2);
+        b = (EditText) findViewById(R.id.editText3);
+        tol = (EditText) findViewById(R.id.editText4);
+        x = (EditText) findViewById(R.id.editText5);
+        n = (EditText) findViewById(R.id.editText6);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button_falsa://caso o click seja no botão calcular
+
+                funçãoS = função.getText().toString();   //capturamos o que foi digitado na caixa de texto da função
+                System.out.println(funçãoS);//printamos o calor capturado no console
+                GraphView graph = (GraphView) findViewById(R.id.graph3); // instanciamos o gráfico
+
+                DataPoint[] data = new DataPoint[200];
+                double x = -10,y=0;
+                for(int i =0; i < 200; i++){
+                    y = x*x;
+                    data[i] = new DataPoint(x,y);
+                    x += 0.2;
+                }
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
+                series.setColor(Color.RED);
+                graph.addSeries(series);
+                graph.getViewport().setScalable(true);
+                graph.getViewport().setScrollable(true);
+
+        }
+    }
 }
