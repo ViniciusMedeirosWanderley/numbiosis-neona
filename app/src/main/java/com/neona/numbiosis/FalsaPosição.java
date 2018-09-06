33cf90123420b062d850e381a937f0457ec7aca5
Jayme Welton <jaymequimica@hotmail.com>
2018-09-06 14:38:12 -0600

    Telas H / V
    Agora as telas podem ficar na posição horizontal sem bagunçar a interface.

--- app/src/main/java/com/neona/numbiosis/FalsaPosição.java
+++ app/src/main/java/com/neona/numbiosis/FalsaPosição.java
@@ -1,58 +1,88 @@
 package com.neona.numbiosis;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class FalsaPosição extends AppCompatActivity implements  View.OnClickListener {
-    EditText função , a,b,tol,x,n;
-    String funçãoS,aS,bS,tolS,xS,nS;//variaveis para captação dos dados introduzidos pelo usuário
+    double raiz;
+    EditText função , a,b,tol,n;
+    String funçãoS,aS,bS,tolS,nS;//variaveis para captação dos dados introduzidos pelo usuário
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_falsa_posicao);//tela que estamos usando
 
-        Button button = (Button) findViewById(R.id.button_falsa);//instanciamos o botão da tela
+        Button button = (Button) findViewById(R.id.button_falsaPosição);//instanciamos o botão da tela
         button.setOnClickListener((View.OnClickListener ) this); //colocamos ele pra ser "escutado"
-
-        função = (EditText) findViewById(R.id.editText); //instanciamos as caixas de texto da tela
-        a = (EditText) findViewById(R.id.editText2);
-        b = (EditText) findViewById(R.id.editText3);
-        tol = (EditText) findViewById(R.id.editText4);
-        x = (EditText) findViewById(R.id.editText5);
-        n = (EditText) findViewById(R.id.editText6);
 
     }
 
     @Override
     public void onClick(View view) {
+
+        função = (EditText) findViewById(R.id.editText_função_falsaPosição); //instanciamos as caixas de texto da tela
+        a = (EditText) findViewById(R.id.editText_a_falsaPosição);
+        b = (EditText) findViewById(R.id.editText_b_falsaPosição);
+        tol = (EditText) findViewById(R.id.editText_tol_falsaPosição);
+        n = (EditText) findViewById(R.id.editText_n_falsPosição);
+
+        if(função.getText().toString().equals("")){
+            função.setText("x^2 + x - 6");
+        }
+        if(a.getText().toString().equals("")){
+            a.setText("1.5");
+        }
+        if(b.getText().toString().equals("")){
+            b.setText("1.7");
+        }
+        if(tol.getText().toString().equals("")){
+            tol.setText(getText(R.string.txt_hint_ERRO));
+        }
+        if(n.getText().toString().equals("")) {
+            n.setText(R.string.txt_hint_N);
+        }
+
         switch(view.getId()){
-            case R.id.button_falsa://caso o click seja no botão calcular
+            case R.id.button_falsaPosição://caso o click seja no botão calcular
 
                 funçãoS = função.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
-                System.out.println(funçãoS);//printamos o calor capturado no console
-                GraphView graph = (GraphView) findViewById(R.id.graph3); // instanciamos o gráfico
+                aS = a.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
+                bS = b.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
+                tolS = tol.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
+                nS = n.getText().toString();
 
-                graph.removeAllSeries(); //limpa o gráfico contendo funções já plotadas.
+                double a, b, tol;
+                int n;
+                n = Integer.parseInt(nS);
+                a = Double.parseDouble(aS);
+                b = Double.parseDouble(bS);
+                tol = Double.parseDouble(tolS);
 
-                DataPoint[] data = new DataPoint[200];
-                double x = -10,y=0;
-                for(int i =0; i < 200; i++){
-                    y = x*x;
-                    data[i] = new DataPoint(x,y);
-                    x += 0.2;
+
+                try{
+                    raiz = Raiz.falsaPosicao(funçãoS,a,b,tol,n);
+
+                    Intent intent = new Intent(this,PlotagemActivity.class);
+                    intent.putExtra("funcao", funçãoS);
+                    intent.putExtra("raiz", raiz);
+                    startActivity(intent);
+                }catch (ArithmeticException ex){
+                    Toast.makeText(getApplicationContext(),"Raiz não encontrada.\nTente outro intervalo.",Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent(this,PlotagemActivity.class);
+                    intent.putExtra("funcao", funçãoS);
+                    intent.putExtra("raiz_ok", false);
+                    startActivity(intent);
                 }
-                LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
-                series.setColor(Color.RED);
-                graph.addSeries(series);
-                graph.getViewport().setScalable(true);
-                graph.getViewport().setScrollable(true);
 
         }
     }
--- app/src/main/java/com/neona/numbiosis/Secante.java
+++ app/src/main/java/com/neona/numbiosis/Secante.java
@@ -1,16 +1,17 @@
 package com.neona.numbiosis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 
 public class Secante extends AppCompatActivity implements  View.OnClickListener {
     double raiz;
-    GraphHandler handler;
     EditText função , x0,x1,tol,n;
     String funçãoS,x0S,x1S,tolS,nS;//variaveis para captação dos dados introduzidos pelo usuário
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,6 @@ public class Secante extends AppCompatActivity implements  View.OnClickListener 
 
         Button btn_calcular_secante = (Button) findViewById(R.id.btn_calcular_secante);//instanciamos o botão da tela
         btn_calcular_secante.setOnClickListener((View.OnClickListener ) this); //colocamos ele pra ser "escutado"
-
-
     }
 
     @Override
@@ -32,11 +31,24 @@ public class Secante extends AppCompatActivity implements  View.OnClickListener 
         tol = (EditText) findViewById(R.id.editText_tol_secante);
         n = (EditText) findViewById(R.id.editText_n_secante);
 
+        if(função.getText().toString().equals("")){
+            função.setText("x^2 + x - 6");
+        }
+        if(x0.getText().toString().equals("")){
+            x0.setText("1.5");
+        }
+        if(x1.getText().toString().equals("")){
+            x1.setText("1.7");
+        }
+        if(tol.getText().toString().equals("")){
+            tol.setText(getText(R.string.txt_hint_ERRO));
+        }
+        if(n.getText().toString().equals("")) {
+            n.setText(R.string.txt_hint_N);
+        }
+
         switch(view.getId()){
             case R.id.btn_calcular_secante://caso o click seja no botão calcular
-
-                GraphView graph2 = (GraphView) findViewById(R.id.graph2); // instanciamos o gráfico
-                graph2.removeAllSeries(); //limpa o gráfico contendo funções já plotadas.
 
                 funçãoS = função.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
                 x0S = x0.getText().toString();   //capturamos o que foi digitado na caixa de texto da funcao
@@ -51,14 +63,21 @@ public class Secante extends AppCompatActivity implements  View.OnClickListener 
                 x1 = Double.parseDouble(x1S);
                 tol = Double.parseDouble(tolS);
 
+                try{
+                    raiz = Raiz.secante(funçãoS,x0,x1,tol,n);
 
-                raiz = Raiz.secante(funçãoS,x0,x1,tol,n);
+                    Intent intent = new Intent(this,PlotagemActivity.class);
+                    intent.putExtra("funcao", funçãoS);
+                    intent.putExtra("raiz", raiz);
+                    startActivity(intent);
+                }catch (ArithmeticException ex){
+                    Toast.makeText(getApplicationContext(),"Raiz não encontrada.\nTente outro intervalo.",Toast.LENGTH_LONG).show();
 
-                handler = new GraphHandler(funçãoS, graph2);
-                handler.initSerieFuncao();
-                handler.initSerieRaizes();
-                handler.initSerieRaizFinal(raiz);
-                handler.initGraph();
+                    Intent intent = new Intent(this,PlotagemActivity.class);
+                    intent.putExtra("funcao", funçãoS);
+                    intent.putExtra("raiz_ok", false);
+                    startActivity(intent);
+                }
 
         }
     }
