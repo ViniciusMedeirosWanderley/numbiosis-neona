61b232913e40de5a98640a451896e5f3def4cd7f
Leonardo Villeth <lvilleth@gmail.com>
2018-10-17 10:33:39 -0300

    Passo a passo Katex para Quadratura Gaussiana

--- app/src/main/java/com/neona/numbiosis/QuadGaussSolucaoActivity.java
+++ app/src/main/java/com/neona/numbiosis/QuadGaussSolucaoActivity.java
@@ -3,14 +3,8 @@ package com.neona.numbiosis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.LinearLayout;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-
 import katex.hourglass.in.mathlib.MathView;
 
 public class QuadGaussSolucaoActivity extends AppCompatActivity {
@@ -36,11 +30,9 @@ public class QuadGaussSolucaoActivity extends AppCompatActivity {
         MathView mv = findViewById(R.id.mv_QG_solucao);
         String katex = katexIntegral(a,b,funcao,variavel,resultado);
         mv.setDisplayText(katex);
-
         mv.getLayoutParams().width = katex.length()*18;
 
-
-        //Log.d("func",katexIntegral(a,b,funcao,variavel,resultado));
+        passoApasso();
     }
 
     private String katexIntegral(int inf, int sup, String f, String var, double result){
@@ -51,7 +43,7 @@ public class QuadGaussSolucaoActivity extends AppCompatActivity {
         NumberFormat nf = DecimalFormat.getInstance();
         nf.setMaximumFractionDigits(8);
 
-        return "$$\\large\\int_{"+inf+"}^{"+sup+"}{"+f+"}\\space d"+var+" = +"+nf.format(result)+"$$";
+        return "$$\\large\\int_{"+inf+"}^{"+sup+"}{"+f+"}\\space d"+var+" = \\bold{"+nf.format(result)+"}$$";
     }
 
     private String descobreVariavel(String f){
@@ -59,5 +51,34 @@ public class QuadGaussSolucaoActivity extends AppCompatActivity {
                 .replaceAll("e", "")
                 .replaceAll("pi","").charAt(0);
         return var;
+    }
+
+    private String passoApasso(){
+        MathView mv = findViewById(R.id.mv_QG_explica1);
+        String katex = "";
+
+        katex += "$$\\text{Detalhes}$$";
+        katex += "$$\\int_{-1}^{1}f(x)dx\\approx\\sum_{i=0}^nA_if(x_i)$$";
+        katex += "$$A_i:\\text{Pesos de Gauss-Legendre}$$";
+        katex = pulaLinha(katex);
+
+        katex += "$$\\text{Para } [a,b]\\ne{[-1,1]}:$$";
+        katex += "$$\\int_{a}^{b}f(x)dx\\Rightarrow\\int_{-1}^{1}F(t)dt$$";
+
+        katex += "$$\\text{Tal que:}$$";
+        katex += "$$x = \\frac{t\\cdot(b-a) + (b+a)}{2}$$";
+
+        katex += "$$\\downdownarrows$$";
+
+        katex += "$$\\int_{-1}^{1}F(t)dt\\approx\\sum_{i=0}^nA_iF(t_i)$$";
+
+        mv.setDisplayText(katex);
+        mv.getLayoutParams().width = katex.length()*4;
+
+        return katex;
+    }
+
+    private String pulaLinha(String s){
+        return s+"$$$$";
     }
 }
